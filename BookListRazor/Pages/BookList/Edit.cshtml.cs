326f1501f78a3baa5cf56dd4205141fe3b7c2b98
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2021-02-05 15:49:05 +0100

    Create and Edit logic added

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -8,37 +8,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
+        private ApplicationDbContext _db;
 
-        private readonly ApplicationDbContext _db;
-
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
-
         [BindProperty]
         public Book Book { get; set; }
-        
 
-        public void OnGet()
+        public async Task OnGet(int id)
         {
+            Book = await _db.Book.FindAsync(id);
         }
 
         public async Task<IActionResult> OnPost()
         {
-            if (ModelState.IsValid)
+            if(ModelState.IsValid)
             {
-               await _db.Book.AddAsync(Book);
+                var BookFromDB = await _db.Book.FindAsync(Book.Id);
+                BookFromDB.Name = Book.Name;
+                BookFromDB.Author = Book.Author;
+                BookFromDB.ISBN = Book.ISBN;
+
                 await _db.SaveChangesAsync();
+
                 return RedirectToPage("Index");
-            }
-            else
+            } else
             {
-                return Page();
+                return RedirectToPage();
             }
         }
-
     }
 }
