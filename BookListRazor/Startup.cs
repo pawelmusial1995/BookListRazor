bf2f6bb3d582f9516ee579e044958b6d265fec55
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2021-02-05 17:11:14 +0100

    Add DataTables framework and fix edit and remove logic

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -26,7 +26,9 @@ namespace BookListRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
